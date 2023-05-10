59700473135d162f718b220f3142fc89b37094e1
Sehun <62082124+Sehun-github@users.noreply.github.com>
2023-05-10 09:57:59 +0900

    Add files via upload

---
+++ R/file/자료구조.R
@@ -0,0 +1,12 @@
+vector("character", length = 5)
+vector("numeric", length = 5)
+vector("logical", length = 5)
+class(c("1","3"))
+class(c(1.7,"3"))
+class(c(TRUE,3))
+1L+2L
+matrix(nrow = 2, ncol = 3)
+class(matrix(nrow=2,ncol=3))
+array(dim=c(2,2,3))
+list(c(1,2,3),T,"hi")
+
